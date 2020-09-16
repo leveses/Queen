418ae284290565c1a9145246639f0d4a0515ebc2
leveses <boda.zsombor10@gmail.com>
2020-09-16 09:01:36 +0200

    Konstruktor

--- Queen/Program.cs
+++ Queen/Program.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Queen
 {
     class Tabla
     {
-        char[,] tomb = new char[8,8];
-        private char UserCella;
-        private int UserOszlopokSzama;
-        private int UserSorokSzama;
+        char[,] tomb;
+        private char UresCella;
+        private int UresOszlopokSzama;
+        private int UresSorokSzama;
 
         public Tabla(char ch)
-        { 
-            
+        {
+            tomb = new char[8, 8];
+            UresCella = ch;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    tomb[i,j] = UresCella;  
+                }
+            }
         }
         static void Elhelyez()
         { 
@@ -29,17 +38,17 @@ namespace Queen
         { 
         
         }
-        static char Tabla()
-        { 
-        
-        }
-        static int UserOszlop()
-        { 
-        
-        }
-        static int UserSor()
+        static char Tabla(char a)
         {
-
+            return a;
+        }
+        static int UresOszlop()
+        {
+            return 0;
+        }
+        static int UresSor()
+        {
+            return 0;
         }
 
 
