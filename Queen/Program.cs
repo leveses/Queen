b1c0e91a98333a9af7981b0236ade7fab8751cfe
leveses <boda.zsombor10@gmail.com>
2020-09-16 08:49:54 +0200

    alap

--- Queen/Program.cs
+++ Queen/Program.cs
@@ -6,6 +6,44 @@ using System.Threading.Tasks;
 
 namespace Queen
 {
+    class Tabla
+    {
+        char[,] tomb = new char[8,8];
+        private char UserCella;
+        private int UserOszlopokSzama;
+        private int UserSorokSzama;
+
+        public Tabla(char ch)
+        { 
+            
+        }
+        static void Elhelyez()
+        { 
+        
+        }
+        static void FajlbaIr()
+        { 
+        
+        }
+        static void Megjelenit()
+        { 
+        
+        }
+        static char Tabla()
+        { 
+        
+        }
+        static int UserOszlop()
+        { 
+        
+        }
+        static int UserSor()
+        {
+
+        }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
